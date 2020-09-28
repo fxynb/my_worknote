dc926e03205f36cc8b901fea010f3df3b516f5fb
wumengqi <1065090086@qq.com>
2020-09-28 10:21:04 +0800

    computed

--- vue/vue_demo/demo_1/src/views/About.vue
+++ vue/vue_demo/demo_1/src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="about">
     <p>Original message: "{{ message }}"</p>
-    <p>Computed reversed message: "{{ reversedMessage }}"</p>
+    <p>Computed reversed message: "{{ reversedMessage() }}"</p>
   </div> 
 </template>
 
@@ -12,12 +12,20 @@ export default {
       message:'hello'
     }
   },
+  //通过computed进行计算
   computed:{
-    //通过computed进行计算
     //默认是getter属性
+    // reversedMessage:function () {
+    //   console.log(this.message.split('').reverse().join(''))  // ["h", "e", "l", "l", "o"]   join() 方法用于把数组中的所有元素放入一个字符串。
+    //     return this.message.split('').reverse().join('')
+    //   console.log(this.message.jion(''))  //报错，不是数组
+    // }
+  },
+
+  //用方法的方式
+  methods:{
     reversedMessage:function () {
-      // console.log(this.message.split(''))  // ["h", "e", "l", "l", "o"]
-      console.log(this.message.jion(''))  //
+      return this.message.split('').reverse().join('')
     }
   }
 }
