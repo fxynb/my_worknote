ea0f097de8bedf47a68d02576b73039a7cfe085e
wumengqi <1065090086@qq.com>
2020-10-12 16:23:04 +0800

    对象转成数组

--- array/arrayToString.js
+++ array/arrayToString.js
@@ -3,5 +3,21 @@ let a = [1, 2, 3]
 
 let s = a.toString()
 
-console.log(a)
-console.log(s)
+// console.log(a)
+// console.log(s)
+
+let b = [1, 2, 3]
+console.log(b.join(','))
+
+
+//将对象转换成数组的形式,比如下a对象所示，我想要获取对象中的key值组成的数组，就可以通过Object.keys(a)返回一个key值的数组 
+
+var m = {
+    1: '123',
+    2: '123',
+    3: '123',
+    4: '123',
+}
+
+
+console.log(Object.keys(m)) //[ '1', '2', '3', '4' ]
